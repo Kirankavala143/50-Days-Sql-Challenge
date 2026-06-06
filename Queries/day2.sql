760e3e83b473c0fc145b88c69f4271d8b2923423
Kiran Kumar Kavala <kavalakiran@gmail.com>
2026-06-06 22:41:29 +0530

    done

--- Queries/day2.sql
+++ Queries/day2.sql
@@ -1,50 +1,50 @@
 -- USE sql_challenge;
 
 -- 1.departments
--- select * from sql_challenge.cleaned_departments
--- where
--- dept_name is null or dept_name = ''
--- or
--- dept_id is null or dept_id = '';
+select * from cleaned_departments
+where
+dept_name is null or dept_name = ''
+or
+dept_id is null or dept_id = '';
 
 -- 2.employees
--- select * from sql_challenge.cleaned_employees
--- where
--- emp_name is null or emp_name = ''
--- or
--- emp_id is null or emp_id= ''
--- or
--- age is null or age = ''
--- or
--- city is null or city = ''
--- or
--- dept_id is null or dept_id = '';
+select * from cleaned_employees
+where
+emp_name is null or emp_name = ''
+or
+emp_id is null or emp_id= ''
+or
+age is null or age = ''
+or
+city is null or city = ''
+or
+dept_id is null or dept_id = '';
 
 -- 3.salaries
 
--- select * from sql_challenge.cleaned_salaries
--- where
--- salary is null or salary = ''
--- or
--- salary_id is null or salary_id = ''
--- or
--- emp_id is null or emp_id = ''
--- or
--- salary_date is null or salary_date = '';
+select * from cleaned_salaries
+where
+salary is null or salary = ''
+or
+salary_id is null or salary_id = ''
+or
+emp_id is null or emp_id = ''
+or
+salary_date is null or salary_date = '';
 
 -- 4.attendance
--- select * from sql_challenge.cleaned_attendance
--- where    
--- attendance_id is null or attendance_id = ''
--- or
--- emp_id is null or emp_id = ''
--- or
--- attendance_date is null or attendance_date = ''
--- or
--- status is null or status = '';
+select * from cleaned_attendance
+where    
+attendance_id is null or attendance_id = ''
+or
+emp_id is null or emp_id = ''
+or
+attendance_date is null or attendance_date = ''
+or
+status is null or status = '';
 
 -- 5.performance
-select * from sql_challenge.cleaned_performance
+select * from cleaned_performance
 where
 emp_id is null or emp_id = ''
 or

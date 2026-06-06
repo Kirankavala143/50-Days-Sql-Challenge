8fae585a0d193a8612868bef0f08f5ae7b5aaf68
Kiran Kumar Kavala <kavalakiran@gmail.com>
2026-06-06 19:21:24 +0530

    created new tables

--- Queries/table_creation.sql
+++ Queries/table_creation.sql
@@ -1,41 +1,63 @@
+-- use sql_challenge;
+
+-- create table departments
+-- (
+--    dept_id VARCHAR(40),
+--     dept_name VARCHAR(40) 
+-- );
+
+-- create table employees
+-- (
+--     emp_id VARCHAR(40),
+--     emp_name VARCHAR(40),
+--     age VARCHAR(40),
+--     city VARCHAR(40),
+--     dept_id VARCHAR(40),
+--     hire_date VARCHAR(40)
+-- );
+
+-- create table salaries
+-- (
+--     salary_id VARCHAR(40),
+--     emp_id VARCHAR(40),
+--     salary VARCHAR(40),
+--     salary_date VARCHAR(40)
+-- );
+
+-- create table attendance
+-- (
+--     attendance_id VARCHAR(40),
+--     emp_id VARCHAR(40),
+--     attendance_date VARCHAR(40),
+--     status VARCHAR(40)
+-- );
+
+-- create table performance(
+--     emp_id VARCHAR(40),
+--     rating_2022 VARCHAR(40),
+--     rating_2023 VARCHAR(40),
+--     rating_2024 VARCHAR(40)
+
+-- );
+
 use sql_challenge;
+-- create table cleaned_departments
+-- as
+-- select * from departments;
 
-create table departments
-(
-   dept_id VARCHAR(40),
-    dept_name VARCHAR(40) 
-);
+create table cleaned_employees
+as
+select * from employees;
 
-create table employees
-(
-    emp_id VARCHAR(40),
-    emp_name VARCHAR(40),
-    age VARCHAR(40),
-    city VARCHAR(40),
-    dept_id VARCHAR(40),
-    hire_date VARCHAR(40)
-);
+create table cleaned_salaries
+as
+select * from salaries;
 
-create table salaries
-(
-    salary_id VARCHAR(40),
-    emp_id VARCHAR(40),
-    salary VARCHAR(40),
-    salary_date VARCHAR(40)
-);
+create table cleaned_attendance
+as
+select * from attendance;
 
-create table attendance
-(
-    attendance_id VARCHAR(40),
-    emp_id VARCHAR(40),
-    attendance_date VARCHAR(40),
-    status VARCHAR(40)
-);
+create table cleaned_performance
+as
+select * from performance;
 
-create table performance(
-    emp_id VARCHAR(40),
-    rating_2022 VARCHAR(40),
-    rating_2023 VARCHAR(40),
-    rating_2024 VARCHAR(40)
-
-);

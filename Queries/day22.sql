a93e81e68ba69604b4c7bec9c0aedbe27e1835ea
Kiran Kumar Kavala <kavalakiran@gmail.com>
2026-06-26 20:57:46 +0530

    day 22 completed

---
+++ Queries/day22.sql
@@ -0,0 +1,30 @@
+-- use sql_challenge;
+
+-- 1. Department Average Salary (Show each employee with department average salary)
+SELECT
+    e.emp_id,
+    e.dept_id,
+    s.salary,
+    AVG(s.salary) OVER (PARTITION BY e.dept_id) AS dept_avg_salary
+FROM cleaned_employees e
+JOIN cleaned_salaries s
+ON e.emp_id = s.emp_id;
+
+-- 2. Total Salary of Each Department
+SELECT
+    e.emp_id,
+    e.dept_id,
+    s.salary,
+    SUM(s.salary) OVER (PARTITION BY e.dept_id) AS dept_total_salary
+FROM cleaned_employees e
+JOIN cleaned_salaries s
+ON e.emp_id = s.emp_id;
+
+-- 3. Average Performance Rating of Each Department
+SELECT
+    emp_id,
+    dept_id,
+    AVG((rating2022 + rating2023 + rating2024) / 3)
+        OVER (PARTITION BY dept_id) AS dept_avg_rating
+FROM cleaned_performance;
+
